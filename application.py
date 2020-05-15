858b70644e797ef1c3ef8e27960ec88c98e74a32
Achilles <achipap@hotmail.com>
2020-05-15 20:28:18 +0200

    Update application.py

--- application.py
+++ application.py
@@ -355,14 +355,14 @@ def continents():
 
     for x in range(6):
         data={}
-        data['continent'] = cont[x].find_all('td')[0].text.strip('\n')
-        data['total_cases'] = cont[x].find_all('td')[1].text.replace(',', '')
-        data['new_cases'] = cont[x].find_all('td')[2].text.replace(',', '')
-        data['total_deaths'] = cont[x].find_all('td')[3].text.replace(',', '')
-        data['new_deaths'] = cont[x].find_all('td')[4].text.replace(',', '')
-        data['total_recovered'] = cont[x].find_all('td')[5].text.replace(',', '')
-        data['active_cases'] = cont[x].find_all('td')[6].text.replace(',', '')
-        data['serious_cases'] = cont[x].find_all('td')[7].text.replace(',', '')
+        data['continent'] = cont[x].find_all('td')[1].text.strip('\n')
+        data['total_cases'] = cont[x].find_all('td')[2].text.replace(',', '')
+        data['new_cases'] = cont[x].find_all('td')[3].text.replace(',', '')
+        data['total_deaths'] = cont[x].find_all('td')[4].text.replace(',', '')
+        data['new_deaths'] = cont[x].find_all('td')[5].text.replace(',', '')
+        data['total_recovered'] = cont[x].find_all('td')[6].text.replace(',', '')
+        data['active_cases'] = cont[x].find_all('td')[7].text.replace(',', '')
+        data['serious_cases'] = cont[x].find_all('td')[8].text.replace(',', '')
         output.append(data)
 
     parameter = request.args.get('continent')
